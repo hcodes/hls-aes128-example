73c61a9c3f3ef100644d7ca6dfbae4d0dcbe2dd6
Denis Seleznev <hcodes@users.noreply.github.com>
2023-08-04 02:30:08 +0300

    Update

--- 264-fmpeg-aes/openssl.sh
+++ 264-fmpeg-aes/openssl.sh
@@ -15,7 +15,7 @@ encryptedSplitFilePrefix="enc/"
 
 numberOfTsFiles=`ls ${splitFilePrefix}*.m4s | wc -l`
 
-for (( i=1; i<$numberOfTsFiles; i++ ))
+for (( i=0; i<$numberOfTsFiles; i++ ))
 do
 openssl aes-128-cbc -e -in ${splitFilePrefix}$i.m4s -out ${encryptedSplitFilePrefix}$i.m4s -nosalt -iv $initializationVector -K $encryptionKey
 
